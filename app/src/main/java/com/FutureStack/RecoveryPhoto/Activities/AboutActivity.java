dbfa9bea4b033d715793b3b2e7c4203641b54c92
Calavera <yusarctica@gmail.com>
2019-12-10 18:24:49 +0100

    Initial commit

---
+++ app/src/main/java/com/FutureStack/RecoveryPhoto/Activities/AboutActivity.java
@@ -0,0 +1,46 @@
+package com.FutureStack.RecoveryPhoto.Activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.FutureStack.RecoveryPhoto.R;
+
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setupActionBar();
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                finish();
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+
+
+
+}
